ede2c1ed377f46d689e8cb71ed3b03aeeeed224f
tudoushell <whitehatzk520@gmail.com>
2018-10-08 23:03:34 +0800

    完成点击链接轮播图

---
+++ project1/js/picture_change.js
@@ -0,0 +1,81 @@
+//获取轮播图的超链接
+var banner_link = document.querySelectorAll(".banner_link"),
+    a = banner_link[0].getElementsByTagName("a"),
+    banner = document.querySelectorAll(".banner"),
+    ul = banner[0].getElementsByTagName("ul"),
+    li = ul[0].getElementsByTagName("li"),
+    timer;
+ul[0].style.width = li.length * banner[0].clientWidth + "px";
+
+for(var i = 0; i < a.length; i++){
+    a[i].num = i;
+    bindEvent(a[i],"click",function(){
+      
+      var start = ul[0].offsetLeft;
+      var end = this.num * -940;
+      move(ul[0],start,end,20,function(){});
+      
+    });
+  
+}
+
+
+
+function  move(obj,start,end,speed,callback){
+  var newplace;
+  clearInterval(timer);
+  if(start > end){
+    speed = - speed;
+  }
+  timer = setInterval(function(){
+    var currentplace = obj.offsetLeft;
+    newplace = currentplace + speed;
+    if(start >= end && newplace <= end || start <= end && newplace >= end ){
+      newplace = end;
+    }
+    obj.style.left = newplace + "px";
+    console.log(obj.style.left);
+    if(newplace == end){
+      clearInterval(timer);
+    }
+  },30);
+}
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 用于获取元素的样式
+ * obj: 元素名
+ * Style: 样式名
+ * 
+ */
+function getStyle(obj,Style){
+  if(window.getComputedStyle){
+    return getComputedStyle(obj,null)[Style];
+  }else{
+    return obj.currentStyle[Style];
+  }
+}
+/**
+ * 功能：元素事件绑定
+ * @param {元素名} obj 
+ * @param {对元素的事件} eventStr 
+ * @param {回调函数} callback 
+ */
+function bindEvent(obj,eventStr,callback){
+   if(obj.addEventListener){
+     return obj.addEventListener(eventStr,callback,false);
+   }else{
+     return obj.attachEvent("on"+eventStr,function(){
+        callback.call(obj);
+     });
+   }
+}
